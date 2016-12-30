713aa5deed9a7bee873e2223fe8e074ff341ff62
Palo <pjestrada006@hotmail.com>
2016-12-29 18:38:56 -0600

    Allowed Hosts

--- music_emotion_collector/settings.py
+++ music_emotion_collector/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'bycm46e_91f=ihw@!6!8bf0h*a625+z#lp5f(p^u6^)h%(e7=%'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['54.187.236.200']
 
 
 # Application definition
