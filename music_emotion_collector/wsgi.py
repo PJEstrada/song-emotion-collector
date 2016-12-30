f869874dcbd0c3da6abb6b138c4e0ab4a65b3eea
Palo <pjestrada006@hotmail.com>
2016-12-29 19:22:12 -0600

    gunicorn

--- music_emotion_collector/wsgi.py
+++ music_emotion_collector/wsgi.py
@@ -10,7 +10,7 @@ https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-
+#a
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "music_emotion_collector.settings")
 
 application = get_wsgi_application()
